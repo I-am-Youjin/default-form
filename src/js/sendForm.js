2650bcbb8583de83cff540a8b038646acae87d99
I-am-Youjin <atupenko@gmail.com>
2025-01-25 02:13:39 +0300

    fix inputs clearing

--- src/js/sendForm.js
+++ src/js/sendForm.js
@@ -4,20 +4,39 @@ import { values } from "./formValidation";
 const sendBtn = document.getElementById("btn-send");
 const popUp = document.getElementById("pop-up");
 const popUpMessege = document.getElementById("messege");
+const inputName = document.getElementById("input-name");
+const inputEmail = document.getElementById("input-email");
+const inputPhone = document.getElementById("input-phone");
+const inputText = document.getElementById("input-text");
+
+const handleClearInputs = () => {
+  inputName.value = "";
+  inputEmail.value = "";
+  inputPhone.value = "";
+  inputText.value = "";
+  values.name = "";
+  values.email = "";
+  values.phone = "";
+  values.messege = "";
+};
 
 const handleSendForm = async () => {
-  const resp = await fetch("example.com", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(values),
-  });
-  const json = await resp.json();
-  if (json.status === "success") {
-    popUpMessege.innerHTML = json.msg;
-  } else {
-    popUpMessege.innerHTML = json.fields.inputName;
+  try {
+    const resp = await fetch("example.com", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(values),
+    });
+    const json = await resp.json();
+    if (json.status === "success") {
+      popUpMessege.innerHTML = json.msg;
+    } else {
+      popUpMessege.innerHTML = json.fields.inputName;
+    }
+  } catch (e) {
+    popUpMessege.innerHTML = e;
   }
 };
 
@@ -33,6 +52,7 @@ sendBtn.addEventListener("click", () => {
   if (validationStatus) {
     handleSendForm();
     handleShowPopUp();
+    handleClearInputs();
   }
 });
 
